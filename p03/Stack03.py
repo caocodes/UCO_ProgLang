2218e2aaf6021c8879f08b0cba94d79373c11207
Cao Truong <ctruong3@uco.edu>
2022-02-27 14:41:25 -0600

    final p03

--- p03/Stack03.py
+++ p03/Stack03.py
@@ -23,13 +23,21 @@ class Stack():
 
 	def pop(self):
 		return self.items.pop()
-
-	def get_stack(self):
-		return self.items
+	
+	def peek(self):
+		if not self.is_empty():
+			return self.items[-1]
 
 	def is_empty(self):
 		return self.items == []
 
-	def peek(self):
-		if not self.is_empty():
-			return self.items[-1]
+	def size(self):
+		return len(self.items)
+	
+	def print(self):
+		print(*self.items, sep = ", ") 
+
+
+
+
+
--- p03/p03.py
+++ p03/p03.py
@@ -17,107 +17,59 @@
 # Project Account Number: tt035
 #----------------------------------------------------------------------
 
-# Python3 code to Check for 
-# balanced parentheses in an expression 
-
+import traceback
 import sys
-import string
 from Stack03 import Stack
 
+brackets = {
+	"(": ")",
+	"[": "]",
+	"{": "}",
+}
 
-# Function to check parentheses 
-def check(paranthese_start, paranthese_end): 
-	if paranthese_start == "(" and paranthese_end == ")":
-		return True
-	elif paranthese_start == "[" and paranthese_end == "]":
-		return True
-	elif paranthese_start == "{" and paranthese_end == "}":
-		return True
-	else:
-		return False
-
-#Function to check for whitespaces
-def check_whitespace(str_space):
-	for whitespace in str_space:
-		if whitespace in string.whitespace:
-			return True
-	return False
-
-# This function checks each string input with itself using the Stack 
-# implementation made in Stack03.py so that each letter that matched with the 
-# open and close brackets, paranthese, and curly braces are put in there and 
-# then compared with each stach push and pop.
-def check_string(instr):
-	stack_balance = Stack()
-	balance = True
-	i = 0
-
-	while i < len(instr) and balance:
-		paranthese = instr[i]
-		if paranthese in "({[]})":
-			if paranthese in "({[":
-				stack_balance.push(paranthese)
-				#print("pushed ", paranthese)
-			elif paranthese in ")}]":
-				if stack_balance.is_empty():
-					balance = False
-				else:
-					top = stack_balance.pop()
-					#print("popped ", top, paranthese)
-					if not check(top, paranthese):
-						balance = False
-						#print("balance is ", balance)
-					else:
-						balance = True
-						#print("balance is ", balance)
+def check_str(str):
+	stack = Stack()
+	balance_str = " is balanced."
+	unbalance_str = " is not balanced."
+	for char in str:
+		if char in brackets:
+			stack.push(char)
+		elif char in brackets.values():
+			if not stack.is_empty() and char == brackets[stack.peek()]:
+				stack.pop()
 			else:
-				if stack_balance.is_empty():
-					balance = False
-					#print("balance is ", balance)
-		i += 1
-	if stack_balance.is_empty() and balance:
-		return True
+				return str + unbalance_str
+	if stack.is_empty():
+		return str + balance_str
 	else:
-		return False
+		return str + unbalance_str
 
-# Main function to get input and output files and write to the outputfile
-# of whether the set of strings for each line in the input file has a 
-# balanced set of parantheses, brackets, and curly bracess
-try:
-	if len(sys.argv) == 1:
-		infile = input("Enter input file name:")
-		ofile = input("Enter output file name:")
-	elif len(sys.argv) == 2:
-		infile = sys.argv[1]
-		ofile = input("Enter output file name:")
-	elif len(sys.argv) == 3:
-		infile = sys.argv[1]
-		ofile = sys.argv[2]
-	else:
-		raise Exception('Too many line arguments')
+def process(in_path, out_path):
+	in_file = open(in_path, "r")
+	out_file = open(out_path, "w")
+	while line := in_file.readline():
+		out_file.write(check_str(line.strip()) + "\n")
+	out_file.close()
+	in_file.close()
 
-	count = 0
-	with open(infile, 'r') as fp:
-		saveFile = open(ofile, 'w')
-		for count, line in enumerate(fp):
-			fline = line.strip()
-			if(not fline):
-				temp = fline + " Blank line, not a string. \n"
-				saveFile.write(temp)
-			elif check_whitespace(fline):
-				temp = fline + " contains whitespace, not a string. \n"
-				saveFile.write(temp)
-			elif check_string(fline):
-				temp = fline + " is balanced. \n"
-				saveFile.write(temp)
-			else:
-				temp = fline + " is not balanced. \n"
-				saveFile.write(temp)
-			count += 1
-	saveFile.close()
-
-except Exception as e:
-	template = "An exception of type {0} occurred."
-	message = template.format(type(e).__name__)
-	print(message)
-	print(str(e))
+if __name__ == '__main__':
+	try:
+		args = sys.argv[1:]
+		in_msg = "Enter the input file name: "
+		out_msg = "Enter the output file name: "
+		match len(args):
+			case 0:
+				in_path = input(in_msg)
+				out_path = input(out_msg)
+			case 1:
+				in_path = args[0]
+				out_path = input(out_msg)
+			case 2:
+				in_path = args[0]
+				out_path = args[1]
+			case _:
+				raise ValueError("Arguments count > 2")
+		process(in_path, out_path)
+	except Exception as e:
+		print("Encountered exception type: {0}".format(type(e).__name__))
+		print(traceback.format_exc())
