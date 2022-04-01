2bc39ca59331a3245c0291e321008932b83e537a
Cao Truong <ctruong3@uco.edu>
2022-03-31 19:25:15 -0500

    final clean

--- p04_5/pasm_label.cpp
+++ p04_5/pasm_label.cpp
@@ -1,3 +1,20 @@
+//--------------------------------------------------------------------
+// File pasm_label.cpp contains the implementation for the 
+// PasmLabel utility class for processing P-Code labels
+//--------------------------------------------------------------------
+// Author 1: Prantosh Neupane
+// Student ID: *20514054
+// E-Mail:	pneupane4@uco.edu
+// CRN:		21808, Spring 2022
+// Author 2: Cao Truong
+// Student ID: *20500231
+// E-Mail:	ctruong3@uco.edu
+// CRN:		22708, Spring 2022
+// Course:	CMSC 5023 – Programming Languages
+// Project:	p04.5
+// Due:		March 31, 2022
+// Project Account Number: tt035
+//--------------------------------------------------------------------
 #include "pasm_label.h"
 
 PasmLabel::PasmLabel(string lbl, int addr, bool res) : labelStr(lbl), address(addr), resolved(res) {}
--- p04_5/pasm_label.h
+++ p04_5/pasm_label.h
@@ -1,3 +1,21 @@
+//--------------------------------------------------------------------
+// File pasm_label.h contains the interface for the 
+// PasmLabel utility class for processing P-Code labels
+//--------------------------------------------------------------------
+// Author 1: Prantosh Neupane
+// Student ID: *20514054
+// E-Mail:	pneupane4@uco.edu
+// CRN:		21808, Spring 2022
+// Author 2: Cao Truong
+// Student ID: *20500231
+// E-Mail:	ctruong3@uco.edu
+// CRN:		22708, Spring 2022
+// Course:	CMSC 5023 – Programming Languages
+// Project:	p04.5
+// Due:		March 31, 2022
+// Project Account Number: tt035
+//--------------------------------------------------------------------
+
 #ifndef pasm_label_h
 #define pasm_label_h
 
